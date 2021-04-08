16ba9f63cf572128be04050e6d9e29649356358d
alifazani <81021209+alifazani@users.noreply.github.com>
2021-04-09 00:02:00 +0800

    Final

--- Test.py
+++ Test.py
@@ -128,11 +128,11 @@ while True:
     print("+++++++++++++++++++++++++++++++++")
     print("1. Create List")
     print("2. Insert in beginning")
-    print("3. Count element in the List")
-    print("4. Enter Item to be deleted")
-    print("5. Display")
-    print("6. Insert at tail")
-    print("7. List of element in forwand and reverse")
+    print("3. Insert in tail")
+    print("4. Count element in the List")
+    print("5. Enter Item to be deleted")
+    print("6. List of element in forward and reverse")
+    print("7. Display")
     print("8. Display min")
     print("9. Display max")
     print("10. Exit")
@@ -149,21 +149,21 @@ while True:
         item = int(input("Enter number to add to the list: "))
         myLList.add(item)
     elif choice == 3:
-        print("Number of element(s) in the linked list: ", myLList.count())
-    elif choice == 4:
-        number = int(input("Enter number you want to detele: "))
-        myLList.delete(number)
-    elif choice == 5:
-        print("List of element(s):")
-        myLList.printList()
-    elif choice == 6:
         item = int(input("Enter number to add to the list: "))
         myLList.addtail(item)
-    elif choice == 7:
+    elif choice == 4:
+        print("Number of element(s) in the linked list: ", myLList.count())
+    elif choice == 5:
+        number = int(input("Enter number you want to detele: "))
+        myLList.delete(number)
+    elif choice == 6:
         print("Forward: ")
         myLList.printList()
         print("\nReverse: ")
         myLList.reverse()
+    elif choice == 7:
+        print("List of element(s):")
+        myLList.printList()
     elif choice == 8:
         print("The node with minimum value is : ", myLList.min_node())
     elif choice == 9:
